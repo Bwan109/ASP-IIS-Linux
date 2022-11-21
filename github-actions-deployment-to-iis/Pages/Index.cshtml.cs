cf8c8ce51cef700f3b68cdf27b558be864ebf465
Bwan109 <brandon@virtuosity-global.com>
2022-11-21 13:34:02 +0800

    first

---
+++ github-actions-deployment-to-iis/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace github_actions_deployment_to_iis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
